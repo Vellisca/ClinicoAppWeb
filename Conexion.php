9f301eeeb98af517021bfa38cfbcdd08c7fd6fc6
Vellisca <146184942+Vellisca@users.noreply.github.com>
2023-10-15 12:57:56 +0200

    Add files via upload

--- Conexion.php
+++ Conexion.php
@@ -1,17 +1,63 @@
 <?php
-$servername= "localhost";
-$username ="";
-$password="";
-$database="";
 
-//Creamos la conexion
-$conn = mysqli_connect($servername,$username,$password,$database);
+session_start();
 
-//Verificamos la conexion
-if (!$conn){
-die ("No se ha podido conectar con la base de datos" . mysqli_connect_error());
+if( isset($_SESSION['user_id']) ){
+	header("Location: /");
 }
 
-echo "Conectado";
-mysqli_close($conn);
+require 'database.php';
+
+if(!empty($_POST['email']) && !empty($_POST['password'])):
+	
+	$records = $conn->prepare('SELECT id,email,password FROM users WHERE email = :email');
+	$records->bindParam(':email', $_POST['email']);
+	$records->execute();
+	$results = $records->fetch(PDO::FETCH_ASSOC);
+
+	$message = '';
+
+	if(count($results) > 0 && password_verify($_POST['password'], $results['password']) ){
+
+		$_SESSION['user_id'] = $results['id'];
+		header("Location: /");
+
+	} else {
+		$message = 'Sorry, those credentials do not match';
+	}
+
+endif;
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Below</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="header">
+		<a href="/">Hola</a>
+	</div>
+
+	<?php if(!empty($message)): ?>
+		<p><?= $message ?></p>
+	<?php endif; ?>
+
+	<h1>Login</h1>
+	<span>or <a href="register.php">register here</a></span>
+
+	<form action="login.php" method="POST">
+		
+		<input type="text" placeholder="Enter your email" name="email">
+		<input type="password" placeholder="and password" name="password">
+
+		<input type="submit">
+
+	</form>
+
+</body>
+</html>
